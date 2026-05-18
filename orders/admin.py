9260aeb8f2b81ef1f08ab1a35a10a341b4728b32
iamnderitum <iamnderitum@gmail.com>
2026-05-18 12:07:08 -0700

    Webhook implementation to receive payment notifications

--- orders/admin.py
+++ orders/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Order, OrderItem
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+def order_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ""
+order_payment.short_descripetion = "Stripe payment"
+
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ["product"]
@@ -17,6 +25,7 @@ class OrderAdmin(admin.ModelAdmin):
         'postal_code',
         'city',
         'paid',
+        order_payment,
         'created',
         'updated'
     ]
