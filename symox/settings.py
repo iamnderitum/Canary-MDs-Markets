fd4ded0410a38e9036f39223d9052fee3482d082
iamnderitum <iamnderitum@gmail.com>
2026-06-02 04:49:20 -0700

    Finalized Pdf generation using weasy print

--- symox/settings.py
+++ symox/settings.py
@@ -159,10 +159,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'assets'
 
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
