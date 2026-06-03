7066d992c1bd250424f5725d0ab33ba8d820de07
iamnderitum <iamnderitum@gmail.com>
2026-06-03 03:53:25 -0700

    Including email credentials in .env file

--- symox/settings.py
+++ symox/settings.py
@@ -202,9 +202,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'moseswambui044@gmail.com'
-EMAIL_HOST_PASSWORD = 'zwcb gpbq rxkx tnwt'
-DEFAULT_FROM_EMAIL = 'moseswambui044@gmail.com'
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 
 #Auth Form Customisation
 ACCOUNT_FORMS = {
