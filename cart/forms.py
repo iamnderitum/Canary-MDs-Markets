111d8123fae7151b8fe964dcfe1d62879016ccf8
iamnderitum <iamnderitum@gmail.com>
2026-06-25 06:28:54 -0700

    Unresolved changes - product page

--- cart/forms.py
+++ cart/forms.py
@@ -5,7 +5,14 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
         choices=PRODUCT_QUANTITY_CHOICES,
-        coerce=int
+        coerce=int,
+        # widget=forms.ChoiceField{
+        #     attrs={
+        #         "class": "form-control",
+
+
+        #     }
+        # }
     )
 
     override = forms.BooleanField(
