3fd6fe6aa94a1b9107d7381dd99b9d75b3d1be1c
iamnderitum <iamnderitum@gmail.com>
2026-07-10 15:44:06 +0300

    # 1 - added shop products Template tags to show - product count

---
+++ shop/templatetags/product_tags.py
@@ -0,0 +1,15 @@
+from django import template
+from django.db.models import Count
+import markdown
+from django.utils.safestring import mark_safe
+from ..models import Product
+
+register = template.Library()
+
+@register.simple_tag(name="product_count")
+def total_products():
+    products = Product.objects.all()
+    count = products.count()
+    print("No of Products", count)
+    # Add query by category
+    return Product.objects.count()
