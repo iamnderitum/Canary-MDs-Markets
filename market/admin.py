a84c57e7419e01b41a5f35029add4f4a967d6ac0
iamnderitum <iamnderitum@gmail.com>
2026-07-10 14:59:59 +0300

    Market App Admin setup - CurrencyPair, Candles

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from .models import CurrencyPair, Candle
 # Register your models here.
+
+@admin.register(CurrencyPair)
+class CurrencyPairAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        "display_name",
+        "pip_location",
+        "display_precision",
+        "created_at"
+    ]
+@admin.register(Candle)
+class CandleAdmin(admin.ModelAdmin):
+    list_display = [
+        "pair",
+        "timeframe",
+        "timestamp",
+        "open",
+        "high",
+        "low",
+        "close",
+        "volume",
+        "complete",
+
+    ]
