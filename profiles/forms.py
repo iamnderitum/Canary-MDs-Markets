6ae15f2e9362210215a49d8924024277a72b35b0
iamnderitum <iamnderitum@gmail.com>
2026-08-16 18:01:33 +0300

    Updated forms.py to include all fields

--- profiles/forms.py
+++ profiles/forms.py
@@ -7,24 +7,63 @@ class ProfileForm(forms.ModelForm):
         fields = [
             "image",
             "bio",
+            "city",
+            "address",
+            "dob",
             "phone",
+            "profession", 
         ]
 
         widgets = {
             "bio": forms.Textarea(
                 attrs={
-                    "rows": 4,
+                    "class": "form-control",
                     "placeholder":"Tell us about You..."
                 }
             ),
-            # "city": forms.Textarea(
-            #     attrs=
-            # )
-
+            "image": forms.ClearableFileInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
+            "city": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Enter City",
+                }
+            ),
+            "address": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Enter Address"
+                }
+            ),
+            "dob": forms.DateInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "+254...",
+                }
+            ),
+            
             "phone": forms.TextInput(
                 attrs={
                     "placeholder":"+254..."
                 }
             ),
+            "profession":forms.Select(
+                attrs={
+                    "class": "form-select"
+                }
+            ),
             
+        }
+
+        labels = {
+            "bio": "Bio",
+            "image": "Upload Profile Image",
+            "city": "City",
+            "address": "Address",
+            "dob": "Date Of Birth",
+            "phone": "Phone",
+            "profession": "Profession"
         }
