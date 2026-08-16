a72b67b03901d1414d1fd4114b1bdb37840a9219
iamnderitum <iamnderitum@gmail.com>
2026-08-16 12:45:42 +0300

    Created view for editing the profile Info

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .forms import ProfileForm
 # Create your views here.
-def profile(request):
-    return HttpResponse("hEREYOU ARE")
+
+@login_required
+def profile_edit(request):
+    profile = request.user.profile
+
+    if request.method == "POST":
+        form = ProfileForm(
+            request.POST,
+            request.FILES,
+            instance=profile
+        )
+
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+
+        else:
+            form = ProfileForm(instance=profile)
+
+        return render(
+            request,
+            "apps/contacts/userprofile.html",
+            {
+                "form": form,
+            },
+        )
