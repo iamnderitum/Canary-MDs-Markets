d5e8abd376533ea44141a4bf2c74857bf747ee41
iamnderitum <iamnderitum@gmail.com>
2026-08-16 19:55:14 +0300

    Implemented Custom function to display image on list_view

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Profile
 # Register your models here.
 
@@ -7,9 +8,35 @@ class ProfileAdmin(admin.ModelAdmin):
     model = Profile
     list_display = [
         "user",
+        "profile_image",
         "phone",
         "profession",
         "dob",
         "city",
     ]
-    list_filter = ["profession", "created"]
+    list_filter = [
+        "profession",
+        "created",
+    ]
+    def profile_image(self, obj):
+        if obj.image:
+            return format_html(
+                '<img src="{}" width="60" height="60" '
+                'style=obect-fit:cover; border-radius:50% />',
+                obj.image.url,
+            )
+
+        return "No Image"
+
+    profile_image.short_description = "Profile Image"
+
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html(
+                'img src="{}" width="150" height="150" '
+                'style="object-fit: cover; border-radius: 50;" />',
+                obj.image.url,
+            )
+        return "No Image uploaded"
+
+    image_preview.short_description = "Current Profile Image"
