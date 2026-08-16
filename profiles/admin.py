cb052252446afa3365720d5fad4523c899ea467f
iamnderitum <iamnderitum@gmail.com>
2026-08-16 18:47:11 +0300

    Implemented profiles for Admin Panel Viewing

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Profile
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    list_display = [
+        "user",
+        "phone",
+        "profession",
+        "dob",
+        "city",
+    ]
+    list_filter = ["profession", "created"]
