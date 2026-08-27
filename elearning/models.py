b3204370c3d6e6a2b7e677ff4efd0524d08cde91
iamnderitum <iamnderitum@gmail.com>
2026-08-27 13:59:03 +0300

    Courses and dynamic/polymorphic data setup

--- elearning/models.py
+++ elearning/models.py
@@ -1,3 +1,6 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -45,3 +48,50 @@ class Module(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Content(models.Model):
+    module = models.ForeignKey(
+        Module,
+        related_name="contents",
+        on_delete=models.CASCADE,
+        
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to={
+            "model_in":("text", "video", "image", "file")
+        }
+    )
+    object_id = models.PositiveBigIntegerField()
+    item = GenericForeignKey("content_type", "object_id")
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(
+        User,
+        related_name='%(class)s_related',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=444)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to="elearning/files")
+
+class Image(ItemBase):
+    file = models.FileField(upload_to="elerning/images")
+
+class Video(ItemBase):
+    url = models.URLField()
--- elearning/views.py
+++ elearning/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
-# Create your views here.
+from django.views.generic.list import ListView
+from .models import Course
+
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = "apps/e-learning/manage/course/list.html"
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner = self.request.user)
+
+
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner = self.request.user)
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class OwnerCourseMixin(OwnerMixin):
+    model = Course
+    fields = ["subject", "title", "slug", "overview"]
+    success_url = reverse_lazy("manage_course_list")
+
