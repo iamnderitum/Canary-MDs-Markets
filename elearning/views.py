1b1aa6458bccae4d76604d6ee272a55ed768fab3
iamnderitum <iamnderitum@gmail.com>
2026-08-28 21:47:24 +0300

    Implemented Templates for Courses - Integrated with django - CRUD Operations all functional

--- elearning/views.py
+++ elearning/views.py
@@ -8,16 +8,17 @@ from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     PermissionRequiredMixin
 )
+from django import forms
 from django.views.generic.list import ListView
 from .models import Course
 
-class ManageCourseListView(ListView):
-    model = Course
-    template_name = "apps/courses/manage/course/list.html"
+# class ManageCourseListView(ListView):
+#     model = Course
+#     template_name = "apps/courses/manage/course/list.html"
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        return qs.filter(owner = self.request.user)
+#     def get_queryset(self):
+#         qs = super().get_queryset()
+#         return qs.filter(owner = self.request.user)
 
 
 class OwnerMixin:
@@ -35,14 +36,48 @@ class OwnerCourseMixin(
 ):
     model = Course
     fields = ["subject", "title", "slug", "overview"]
+    widgets = {
+        "title": forms.Textarea(
+            attrs = {
+                "class": "form-control",
+                "placeholder":"Course Title Here"
+            }
+        ),
+        "slug": forms.TextInput(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Course Slug goes Here"
+            }
+        ),
+        "overview": forms.Textarea(
+            attrs = {
+                "class": "form-control",
+                "placeholder": "Course Overview Here"
+            }
+        ),
+        "subject": forms.Select(
+            attrs ={
+                "class": "form-select"
+            }
+        ),
+
+    }
+
+    labels = {
+        "title": "Title",
+        "slug": "Slug 'title_under'",
+        "overview": "Overview",
+        "subject": "Subject"
+    }
     success_url = reverse_lazy("manage_course_list")
 
 class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
-    template_name = "courses/manage/course/form.html"
+    template_name = "apps/courses/manage/course/form.html"
 
 class ManageCourseListView(OwnerCourseMixin, ListView):
-    template_name = "courses/manage/course/list.html"
+    template_name = "apps/courses/manage/course/list.html"
     permission_required = "courses.view_course"
+    # permission_required = "courses.delete_course"
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
     permission_required = "courses.add_course"
@@ -51,5 +86,5 @@ class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
     permission_required = "courses.change_course"
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
-    template_name = "courses/manage/course/delete.html"
+    template_name = "apps/courses/manage/course/delete.html"
     permission_required = "courses.delete_course"
