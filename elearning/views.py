9a4d5124a532a33151465edec2daae8f19ae0922
iamnderitum <iamnderitum@gmail.com>
2026-08-28 22:34:56 +0300

    Implemented Formset 4 managing course modules and their contents

--- elearning/views.py
+++ elearning/views.py
@@ -10,7 +10,40 @@ from django.contrib.auth.mixins import (
 )
 from django import forms
 from django.views.generic.list import ListView
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic.base import TemplateResponseMixin, View
+
+from .forms import ModuleFormSet
 from .models import Course
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = "apps/courses/manage/module/formset.html"
+    course = None
+
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course, data=data)
+
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(
+            Course, id=pk, owner=request.user
+        )
+        return super().dispatch(request, pk)
+
+    def get(self, request, *args,**kwargs):
+        formset = self.get_formset()
+        return self.render_to_response(
+            {"course": self.course, "formset": formset}
+        )
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect("manage_course_list")
+        return self.render_to_response(
+            {"course":self.course, "formset": formset}
+        )
+
 
 # class ManageCourseListView(ListView):
 #     model = Course
