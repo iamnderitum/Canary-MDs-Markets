29f1dadaf380e578c2a8bfb0e825e38a7d15dfa1
iamnderitum <iamnderitum@gmail.com>
2026-09-04 01:55:58 +0300

    Setting up docker & docker compose

---
+++ symox/settings/local.py
@@ -0,0 +1,13 @@
+from decouple import config
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST"),
+    }
+}
---
+++ symox/settings/prod.py
@@ -0,0 +1,3 @@
+REDIS_URL = "redis://cache:6379"
+CACHES["default"]["LOCATION"] = REDIS_URL
+CHANNEL_LAYERS["default"]["config"]["hosts"] = [REDIS_URL]
